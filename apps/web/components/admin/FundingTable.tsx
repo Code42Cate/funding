916db503a4cb323173bd7b730b2fffcbba1aaff4
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-05-04 15:30:52 +0200

    feat: basic admin table

---
+++ apps/web/components/admin/FundingTable.tsx
@@ -0,0 +1,108 @@
+import { FundingOpportunity } from '@prisma/client';
+
+const transactions = [
+  {
+    id: 'AAPS0L',
+    company: 'Chase & Co.',
+    share: 'CAC',
+    commission: '+$4.37',
+    price: '$3,509.00',
+    quantity: '12.00',
+    netAmount: '$4,397.00',
+  },
+  // More transactions...
+];
+
+export default function FundingTable({ fundingOpportunities }: { fundingOpportunities: FundingOpportunity[] }) {
+  console.log(fundingOpportunities);
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Funding Opportunities List</h1>
+          <p className="mt-2 text-sm text-gray-700">A table of all funding opportunities available.</p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Export
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Program
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Start Date
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    End Date
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Type
+                  </th>
+                  <th scope="col" className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {fundingOpportunities.map((opportunity) => (
+                  <tr key={opportunity.url}>
+                    <td className="whitespace-pre-wrap py-2 pl-4 pr-3 text-sm text-gray-700 sm:pl-0">
+                      {opportunity.title}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                      {opportunity.issuer}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500 font-mono">
+                      {new Date(opportunity.startAt).toDateString()}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500 font-mono">
+                      {new Date(opportunity.deadlineAt).toDateString()}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-center text-white">
+                      <span className="px-2 py-1 bg-blue-500 rounded-full">{opportunity.type}</span>
+                    </td>
+                    <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit<span className="sr-only">, {opportunity.url}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
