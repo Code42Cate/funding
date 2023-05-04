5b83319e0220999ba5fd88a1873fea94802933dc
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-05-04 20:51:34 +0200

    feat: scraper timer box

--- apps/web/pages/scraper/[name].tsx
+++ apps/web/pages/scraper/[name].tsx
@@ -1,14 +1,21 @@
+import ScraperTimer from '../../components/admin/ScraperTimer';
 import UptimeCard from '../../components/admin/Uptime';
 import { useRouter } from 'next/router';
+
+const titleMap = {
+  eu: 'EU Scraper',
+};
 
 export function ScraperDetail({ fallback }) {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>Scraper Detail: {router.query.name} </h1>
+    <div className="py-4 px-2 flex flex-col gap-4 w-fit">
+      <h1 className="text-2xl font-medium">{titleMap[(router.query.name as string) ?? 'eu']}</h1>
 
       <UptimeCard />
+
+      <ScraperTimer />
     </div>
   );
 }
