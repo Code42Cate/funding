e4a9566315b035fd173b1962cc9c118d151f4b44
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-05-06 00:09:08 +0200

    docs: add todo

---
+++ apps/web/pages/notifications.tsx
@@ -0,0 +1,15 @@
+export const NotificationsPage = () => {
+  return (
+    <div className="flex flex-col py-6 pl-8 gap-y-6 max-w-full">
+      <h1 className="text-3xl font-semibold">Notifications</h1>
+      TODO:
+      <ul className="list-disc list-inside">
+        <li>View who enabled notifications for what query and how often notifications have been sent</li>
+        <li>View what the last notifications where</li>
+        <li>line graph of sent-out notifications</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NotificationsPage;
