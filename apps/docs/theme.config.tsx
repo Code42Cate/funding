68547881f2a1af0a42fc533578c42520465e4fde
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-06-07 15:00:05 +0200

    feat: add empty docs page

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: (
+    <span>
+      Fun<sub className="align-sub">2</sub>ding
+    </span>
+  ),
+  project: {
+    link: 'https://github.com/Code42Cate/funding',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/Code42Cate/funding',
+  footer: {
+    text: 'Fun2Ding © 2023',
+  },
+};
+
+export default config;
