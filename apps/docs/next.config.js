81f8c9a72f93d43740ed67f7cfbf69c2c7be1d45
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-06-20 16:20:57 +0200

    docs: wip cluster page

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  latex: true,
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  distDir: '../../dist/apps/docs/.next',
+})
